81f9a09dba08c139bfc0bedead89e056711c361c
Sonali Bhatta <sonali24bhatta@gmail.com>
2023-05-30 21:05:14 +0530

    navbar-DOMmanipulate

--- Assignments/Day2/Script/script.js
+++ Assignments/Day2/Script/script.js
@@ -5,21 +5,25 @@ const textarea=document.getElementById('content');
 button1.addEventListener('click',()=>{
     let div=document.getElementById('showable');
     let image=document.createElement('img');
-    image.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgWy3DLSoDNZxaoOiVo3G9I7-fXtRAztlpB8YtYejl&s';
+    image.src='https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/356100/356129.6.jpg';
+    image.style.height='200px';
     div.appendChild(image);
 })
-// button2.addEventListener('click',()=>{
-//     textarea.innerText("This is my text")
-// })
+
 const table=document.querySelector("tbody");
 button2.addEventListener("click",()=>{
     let tr=document.createElement("tr");
     let td1=document.createElement("td");
-    td1.innerText='1';
+    td1.innerText='2023';
     const td2=document.createElement("td");
-    td2.innerText="Sonali Bhatta";
+    td2.innerText="Women's T20 World Cup";
     const td3=document.createElement("td");
-    td3.innerText="Female";
-    tr.append(td1,td2,td3);
+    td3.innerText="South Africa";
+    const td4=document.createElement("td");
+    td4.innerText="Australia";
+    tr.append(td1,td2,td3,td4);
     table.append(tr);
+})
+button3.addEventListener("click",()=>{
+    window.location.reload();
 })
