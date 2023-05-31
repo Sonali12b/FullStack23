fee314589fbe6793eef36a1fcaf16192582aeb99
Sonali12b <sonali24bhatta@gmail.com>
2023-05-31 12:09:56 +0530

    adding button

--- Assignments/Day3/Script/index.js
+++ Assignments/Day3/Script/index.js
@@ -15,7 +15,7 @@ const data=[
         rating:6
     },
     {
-        image:"https://www.pinkvilla.com/images/2023-02/1675757339_kim-da-mi-1432x2048.jpeg",
+        image:"https://kdramadiary.com/wp-content/uploads/2023/02/Soulmate-kdramadiary-1.jpg",
         title:"Soulmate",
         rating:6
     },{
@@ -33,7 +33,7 @@ const movie_box= document.getElementById("show-movie");
 // data.map((item)=>{
 //     const div=document.createElement("div");
 //     const image=document.createElement("img");
-//     image.src=el.image;
+//     image.src=item.image;
 
 // })
 
@@ -45,7 +45,13 @@ for(let i=0;i<data.length;i++){
     movieTitle.innerHTML=data[i].title;
     const movieRating= document.createElement("p");
     movieRating.innerHTML=data[i].rating;
-    div.append(image,movieTitle,movieRating);
+    const button=document.createElement("button");
+    button.innerText="Watchlist";
+    button.addEventListener('click',()=>{
+        alert(`You have selected on ${data[i].title}`)
+    })
+    div.append(image,movieTitle,movieRating,button);
     movie_box.append(div);
+
 
 }
