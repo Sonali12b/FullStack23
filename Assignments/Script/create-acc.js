bde334ac7e6f4ca1c47b6a2688f47827085527dd
Sonali12b <sonali24bhatta@gmail.com>
2023-06-01 23:09:22 +0530

    [create-acc-post]

---
+++ Assignments/Script/create-acc.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  if (email != "" || password != "") {
+    fetch(" http://localhost:5510/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        id: Date.now(),
+        email: email,
+        password: password,
+      }),
+    })
+      .then(res => res.json)
+      .then(data => {
+        console.log(data);
+        // alert("Account Created successfully!!");
+        window.location.href='./login.html';
+      });
+  } else {
+    alert("Please fill up the details!!");
+  }
+});
