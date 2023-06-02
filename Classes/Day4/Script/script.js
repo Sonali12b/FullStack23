0a4c0e264eb5ed55d38faaca6af3fe05bcd8adbc
Sonali12b <sonali24bhatta@gmail.com>
2023-06-02 12:10:32 +0530

    [put-delete-localstorage]

--- Classes/Day4/Script/script.js
+++ Classes/Day4/Script/script.js
@@ -46,12 +46,48 @@ for(let i=0;i<data.length;i++){
     movieTitle.innerHTML=data[i].title;
     const movieRating= document.createElement("p");
     movieRating.innerHTML=data[i].rating;
+    const updatebtn=document.createElement("button");
+    updatebtn.id="up-btn"
+    updatebtn.innerText="Update";
+    updatebtn.addEventListener('click',()=>{
+        const upd_title=prompt("Enter the new title:");
+        const upd_obj={
+            image: data[i].image,
+            title: upd_title,
+            rating:data[i].rating
+        };
+        fetch(`http://localhost:5501/data/${data[i].id}`,{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(upd_obj),
+        })
+        .then((response)=>response.json())
+        .then((data)=>alert("Updated successfully"));
+    });
+    const deletebtn=document.createElement("button");
+    deletebtn.id="up-btn"
+    deletebtn.innerText="Delete";
+    deletebtn.addEventListener('click',()=>{
+        fetch(`http://localhost:5501/data/${data[i].id}`,{
+            method:"delete",
+        })
+        .then((response)=>response.json())
+        .then((data)=>alert("Deleted successfully"));
+    });
     const button=document.createElement("button");
     button.innerText="Watchlist";
     button.addEventListener('click',()=>{
+        alert(`You have added ${data[i].title}`)
+    })
+    const button1=document.createElement("button");
+    button1.innerText="Select";
+    button1.addEventListener('click',()=>{
+        localStorage.setItem("id",JSON.stringify(data[i].id))
         alert(`You have selected on ${data[i].title}`)
     })
-    div.append(image,movieTitle,movieRating,button);
+    div.append(image,movieTitle,movieRating,button,updatebtn,deletebtn,button1);
     movie_box.append(div);
 }
 });
