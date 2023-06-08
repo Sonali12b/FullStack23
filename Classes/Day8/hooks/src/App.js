4c240910d243537d58fde1c357d6a3f66376f451
Sonali12b <sonali24bhatta@gmail.com>
2023-06-08 21:13:07 +0530

    [search-done]

--- Classes/Day8/hooks/src/App.js
+++ Classes/Day8/hooks/src/App.js
@@ -2,11 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Card } from "./components/card";
 import { useEffect, useState } from "react";
-import './components/Card.css'
+import "./components/Card.css";
 
 function App() {
   const [arr, setArr] = useState([]);
-
+  const [newarr, setnewArr] = useState([]);
+  const [searchStr, getSearchStr] = useState("");
+  useEffect(() => {
+    getSearchedData();
+  }, [searchStr]);
   /*getData*/
   const getData = async () => {
     await fetch("http://localhost:3001/users", {
@@ -24,41 +28,103 @@ function App() {
   }, []);
 
   /*Handle Post*/
-  const handlePost= async()=>{
-    const obj={
-      id:"11",
-      title:"Your Name",
-      url:"https://e0.pxfuel.com/wallpapers/972/674/desktop-wallpaper-your-name-kimi-no-na-wa-names-member-and-cosplay-costumes-anime-your-name-live-thumbnail.jpg",
-      thumbnailUrl:"https://i.pinimg.com/originals/51/22/40/512240630643df5ae0a4e2d240974c9f.png"
-    }
-    await fetch("http://localhost:3001/users",{    
-    method:"POST",
-    body: JSON.stringify(obj),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(res => {
-    res.json();
-    getData();
-  })
-  }
+  const handlePost = async () => {
+    const obj = {
+      id: "99",
+      title: "Your Name",
+      url: "https://e0.pxfuel.com/wallpapers/972/674/desktop-wallpaper-your-name-kimi-no-na-wa-names-member-and-cosplay-costumes-anime-your-name-live-thumbnail.jpg",
+      thumbnailUrl: "9/10",
+    };
+    await fetch("http://localhost:3001/users", {
+      method: "POST",
+      body: JSON.stringify(obj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(res => {
+      res.json();
+      getData();
+    });
+  };
 
-/*handleput*/
-const handlePut= async(id)=>{
-  await fetch("http://localhost:3001/users/")
-}  
+  /*handleput*/
+  const handlePut = async id => {
+    const input = prompt("", 0);
+    await fetch(`http://localhost:3001/users/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // id:"19",
+        title: "Name",
+        url: "https://e0.pxfuel.com/wallpapers/972/674/desktop-wallpaper-your-name-kimi-no-na-wa-names-member-and-cosplay-costumes-anime-your-name-live-thumbnail.jpg",
+        thumbnailUrl: input,
+      }),
+    }).then(res => {
+      if (res.ok) {
+        getData();
+        // alert("updated!!");
+      }
+    });
+  };
+
+  /*delete */
+  const handleDelete = async id => {
+    await fetch(`http://localhost:3001/users/${id}`, {
+      method: "DELETE",
+    }).then(res => {
+      if (res.ok) {
+        alert("Deleted");
+        getData();
+      }
+    });
+  };
+
+  /*searching*/
+  const getSearchedData = () => {
+    const newData = arr.filter(item => item.title.includes(searchStr));
+    console.log(newData);
+    setnewArr(newData);
+  };
+  const handleChange = e => {
+    getSearchStr(e.target.value);
+  };
+  console.log(newarr);
 
   return (
     <div className="App">
-      <h2>useState & useEffect</h2>
-      <button onClick={handlePost} className="btn">Add New Data</button>
-
-      {arr.map(el => {
-        return (
-
-          <Card title={el.title} url={el.url} thumbnailUrl={el.thumbnailUrl} onclick />
-        );
-      })}
+      <h2>Movie Info</h2>
+      <div className="upp">
+        <button onClick={handlePost} className="btn">
+          Add New Data
+        </button>
+        <input
+          type="text"
+          placeholder="Search"
+          id="search"
+          value={searchStr}
+          onChange={e => handleChange(e)}
+        />
+        <button onClick={getSearchedData} className="btn">
+          Search
+        </button>
+      </div>
+      {newarr.length > 0 ? (
+        <div>
+          {newarr.map(el => {
+            return (
+              <Card
+                title={el.title}
+                url={el.url}
+                thumbnailUrl={el.thumbnailUrl}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <h1>No data found</h1>
+      )}
     </div>
   );
 }
--- Classes/Day8/hooks/src/components/card.jsx
+++ Classes/Day8/hooks/src/components/card.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
-import './Card.css';
-export const Card = ({title,url,thumbnailUrl,onclick}) => {
-    // const[change,setChange] = useState([]);
-    // const handleclick=()=>{
-    //     setChange([
-    //         1,2,3,4
-    //     ]
-    //     );
-    // }
-// console.log(change);
+import React, { useState } from "react";
+import "./Card.css";
+import "../App";
+export const Card = ({
+  title,
+  url,
+  thumbnailUrl,
+  handleupdate,
+  deletehandle,
+}) => {
+  console.log(title);
+  console.log(url);
+  // const[change,setChange] = useState([]);
+  // const handleclick=()=>{
+  //     setChange([
+  //         1,2,3,4
+  //     ]
+  //     );
+  // }
+  // console.log(change);
   return (
-
-    <div className='card'>
-       {/* { change==="" ?<h1>Card</h1>:<h1>Card selected</h1>}
+    <div className="card">
+      {/* { change==="" ?<h1>Card</h1>:<h1>Card selected</h1>}
         <button className='btn' onClick={handleclick}>Click me!!</button> */}
-        <img src={url} alt="" className="image" />
-        <p className="title">{title}</p>
-        <img src={thumbnailUrl} alt="" className="thumb" />
-        <button onClick={onclick}>Click me</button>
+      <img src={url} alt="" className="image" />
+      <p className="title">{title}</p>
+      <h3>{thumbnailUrl}</h3>
+      <div className="btnn">
+        <button id="btn1" onClick={handleupdate}>
+          update
+        </button>
 
+        <button id="btn2" onClick={deletehandle}>
+          delete
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
