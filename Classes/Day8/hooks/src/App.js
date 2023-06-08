398befdab9ed338e4ec011d42618e2e437d6132b
Sonali12b <sonali24bhatta@gmail.com>
2023-06-08 09:52:17 +0530

    [get-done]

--- Classes/Day8/hooks/src/App.js
+++ Classes/Day8/hooks/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import {Card  } from "./components/card";
+import logo from "./logo.svg";
+import "./App.css";
+import { Card } from "./components/card";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [arr, setArr] = useState([]);
+  const getData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/photos", {
+      method: "GET",
+    })
+      .then(res => res.json())
+      .then(data => {
+        data = data.slice(0, 10);
+        console.log(data);
+        setArr(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <Card/>
+      <h2>useState & useEffect</h2>
+      {arr.map(el => {
+        return (
+          <Card title={el.title} url={el.url} thumbnailUrl={el.thumbnailUrl} />
+        );
+      })}
     </div>
   );
 }
--- Classes/Day8/hooks/src/components/card.jsx
+++ Classes/Day8/hooks/src/components/card.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
-
-export const Card = () => {
-    const[change,setChange] = useState([]);
-    const handleclick=()=>{
-        setChange([
-            1,2,3,4
-        ]
-        );
-    }
-console.log(change);
+import './Card.css';
+export const Card = ({title,url,thumbnailUrl,onclick}) => {
+    // const[change,setChange] = useState([]);
+    // const handleclick=()=>{
+    //     setChange([
+    //         1,2,3,4
+    //     ]
+    //     );
+    // }
+// console.log(change);
   return (
+
     <div className='card'>
-       { change==="" ?<h1>Card</h1>:<h1>Card selected</h1>}
-        <button className='btn' onClick={handleclick}>Click me!!</button>
+       {/* { change==="" ?<h1>Card</h1>:<h1>Card selected</h1>}
+        <button className='btn' onClick={handleclick}>Click me!!</button> */}
+        <img src={url} alt="" className="image" />
+        <p className="title">{title}</p>
+        <img src={thumbnailUrl} alt="" className="thumb" />
+        <button onClick={onclick}></button>
 
     </div>
   )
